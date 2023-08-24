fc4e6880fa8bb92bff3e77b4cb768fc824fc86f5
Jacob Liddle <u1070198@umail.utah.edu>
2023-08-23 19:14:18 -0600

    Actually adding the files I talked about in the last commit

---
+++ My project/Assets/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PolygonCollider2D playerCollider;
+    public int speedModifier = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int left = 90;
+        int leftup = 45;
+        int leftdown = 135;
+        int right = -90;
+        int rightup = -45;
+        int rightdown = -135;
+        int up = 0;
+        int down = 180;
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.eulerAngles = Vector3.forward * leftup;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.eulerAngles = Vector3.forward * leftdown;
+            }
+            else
+            {
+                transform.eulerAngles = Vector3.forward * left;
+            } 
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.eulerAngles = Vector3.forward * rightup;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.eulerAngles = Vector3.forward * rightdown;
+            }
+            else
+            {
+                transform.eulerAngles = Vector3.forward * right;
+            }
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.eulerAngles = Vector3.forward * up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.eulerAngles = Vector3.forward * down;
+        }
+        
+
+        transform.position = transform.position + transform.up * Time.deltaTime * speedModifier;
+    }
+}
